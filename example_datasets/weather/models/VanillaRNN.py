0343aa76a48b2d2ab7ebb2d3e38b0b7753c39f84
Reinhard <reinn.cs@gmail.com>
2024-02-20 18:52:37 +0200

    Test basic RNN

--- example_datasets/weather/models/VanillaRNN.py
+++ example_datasets/weather/models/VanillaRNN.py
@@ -1,6 +1,13 @@
 import torch
 import torch.nn as nn
 
+is_cuda = torch.cuda.is_available()
+
+# If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
+if is_cuda:
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
 
 class Model(nn.Module):
     """
@@ -20,8 +27,11 @@ class Model(nn.Module):
         # build model
         assert self.rnn_type in ['rnn', 'gru', 'lstm']
         if self.rnn_type == "rnn":
-            self.rnn = nn.RNN(input_size=self.enc_in, hidden_size=self.d_model, num_layers=1, bias=True,
-                              batch_first=True, bidirectional=False)
+            # self.rnn = nn.RNN(input_size=self.enc_in, hidden_size=self.d_model, num_layers=1, bias=True,
+            #                   batch_first=True, bidirectional=False)
+
+            self.rnn = nn.RNNCell(input_size=self.enc_in, hidden_size=self.d_model, bias=True)
+
         elif self.rnn_type == "gru":
             self.rnn = nn.GRU(input_size=self.enc_in, hidden_size=self.d_model, num_layers=1, bias=True,
                               batch_first=True, bidirectional=False)
@@ -42,7 +52,10 @@ class Model(nn.Module):
         if self.rnn_type == "lstm":
             _, (hn, cn) = self.rnn(x)
         else:
-            _, hn = self.rnn(x) # b,s,d  1,b,d
+            hn = torch.zeros((x.size()[0], self.d_model)).to(device)
+            for input_t in x.split(1, dim=1):
+                # x_inp = input_t[0].squeeze()
+                hn = self.rnn(input_t.squeeze(), hn) # b,s,d  1,b,d
 
         # decoding
         y = []
@@ -55,9 +68,9 @@ class Model(nn.Module):
         else:
             for i in range(self.pred_len):
                 yy = self.predict(hn)    # 1,b,c
-                yy = yy.permute(1,0,2) # b,1,c
+                # yy = yy.permute(1,0,2) # b,1,c
                 y.append(yy)
-                _, hn = self.rnn(yy, hn)
+                hn = self.rnn(yy, hn)
         y = torch.stack(y, dim=1).squeeze(2) # bc,s,1
 
         return y
